6c048978eac7d248c2c2ff9419d0b0e6ecca2244
Alberto González <algorinetportfolio@gmail.com>
2019-05-03 13:29:15 +0200

    randomBeer added

--- beers/src/components/randomBeer.js
+++ beers/src/components/randomBeer.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
+
 
 export default class randomBeer extends Component {
+  state = {
+    randomBeer: ''
+  }
+
+  componentDidMount() {
+    console.log("randomBeer montado")
+    axios.get("http://localhost:5000/random").then(randomBeerPayload => {
+      const random = randomBeerPayload.data;
+      console.log(random)
+
+      this.setState({
+        ...this.state,
+        randomBeer: random
+      });
+    });
+  }
   render() {
     return (
       <div>
-        <h1>Random beer</h1>
+        <h1>{this.state.randomBeer.name}</h1>
+            <h5>{this.state.randomBeer.tagline}</h5>
+            <img src={this.state.randomBeer.image_url} alt="" width="100px" />
+            <p>{this.state.randomBeer.contributed_by}</p>
       </div>
     )
   }
